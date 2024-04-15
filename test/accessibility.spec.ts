23d59eff1df2a6cdbc98ab227409feac4ecc7be2
mitchelstarc101 <mitchelstarc101@gmail.com>
2024-04-15 22:31:50 +0530

    commit

--- test/accessibility.spec.ts
+++ test/accessibility.spec.ts
@@ -1,12 +1,4 @@
 import { test, expect } from "@playwright/test";
-<<<<<<< HEAD
-const AxeBuilder = require("@axe-core/playwright");
-
-test.beforeEach(async ({ page }) => {
-  await page.goto("http://demo.opencart.com/");
-});
-test.afterEach(async ({ page }) => {
-=======
 import AxeBuilder from '@axe-core/playwright';
 
 test('Check for WCAG A and AA violations on BBC website', async ({ page }) => {
@@ -35,21 +27,5 @@ test('Check for WCAG A and AA violations on BBC website', async ({ page }) => {
   }
 
   // Close the page (optional, as Playwright often handles closing automatically)
->>>>>>> origin/devtest
   await page.close();
-});
-
-test("Detect Accessibility issues in entire page...", async ({ page }) => {
-  const scanResutls = await new AxeBuilder({ page }).analyze();
-  //Console log the violations
-  let violation = scanResutls.violations;
-  violation.forEach(function (entry) {
-    console.log(
-      "Print the Violations:",
-      entry.impact + " " + entry.description
-    );
-  });
-  let count = violation.length;
-  console.log("List of Violations:", count);
-  expect(count).toEqual(5);
 });
