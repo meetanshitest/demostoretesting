13547ffb2db1ba46244a1fd8b8059fabcd6acc46
mitchelstarc101 <mitchelstarc101@gmail.com>
2024-04-15 22:48:40 +0530

    commit format document

--- test/demoOne.spec.ts
+++ test/demoOne.spec.ts
@@ -5,18 +5,16 @@ import { firstDemoCheckout } from "../pages/firstDemoCheckout";
 test.describe("First Demo store test cases", () => {
   let browser: Browser;
   let page: Page;
-   
+
   test.beforeAll(async () => {
     browser = await chromium.launch({
-      headless:false
-
+      headless: false,
     });
-    const context=await browser.newContext({
-      recordVideo:{
-        dir:"./videos/"
-      }
-    })
-
+    const context = await browser.newContext({
+      recordVideo: {
+        dir: "./videos/",
+      },
+    });
   });
 
   test.beforeEach(async () => {
@@ -27,54 +25,50 @@ test.describe("First Demo store test cases", () => {
   test.afterAll(async () => {
     await browser.close();
   });
-test.skip("Verify Category page Heading",async()=>{
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.navigateToCategoryPage()
-})
-test.skip("Verify Product page Heading", async () => {
-    const firstDemo=new firstDemoCheckout(page);
+  test.skip("Verify Category page Heading", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.navigateToCategoryPage();
+  });
+  test.skip("Verify Product page Heading", async () => {
+    const firstDemo = new firstDemoCheckout(page);
     await firstDemo.navigateToProductPage();
+  });
+  test.skip("Verify Add To Cart Button", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.addProductInCart();
+  });
+  test.skip("Verify Add To Cart Success Message", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.verifySuccessMsg();
+  });
+  test.skip("Check Price is visible or not", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.verifyPrice();
+  });
+  test.skip("Check Shopping cart Link", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.navigateToCart();
+  });
 
-});
-test.skip("Verify Add To Cart Button", async () => {
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.addProductInCart();
-    
-});
-test.skip("Verify Add To Cart Success Message", async () => {
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.verifySuccessMsg();
-    
-});
-test.skip('Check Price is visible or not',async()=>{
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.verifyPrice();
-})
-test.skip("Check Shopping cart Link",async()=>{
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.navigateToCart();
-})
-
-test.skip('Check SignIn link',async()=>{
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.verifySignInLink();
-})
-test.skip('Check Create Account link',async()=>{
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.verifyCreateAccountLink();
-})
-test.skip('navigate To Checkout page',async()=>{
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.navigateToCheckout();
-})
-test.skip("Check Update Cart based on condition",async()=>{
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.navigateToCheckout()
-})  
-test('Check place order',async()=>{
-  const firstDemo=new firstDemoCheckout(page);
-  await firstDemo.placeOrder();
-  await page.waitForTimeout(2000)
-})
-  
+  test.skip("Check SignIn link", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.verifySignInLink();
+  });
+  test.skip("Check Create Account link", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.verifyCreateAccountLink();
+  });
+  test.skip("navigate To Checkout page", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.navigateToCheckout();
+  });
+  test.skip("Check Update Cart based on condition", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.navigateToCheckout();
+  });
+  test("Check place order", async () => {
+    const firstDemo = new firstDemoCheckout(page);
+    await firstDemo.placeOrder();
+    await page.waitForTimeout(2000);
+  });
 });
