b2fa2adbda47e52ac394902d1f6863803472418c
mitchelstarc101 <mitchelstarc101@gmail.com>
2024-06-23 12:30:33 +0530

    update yml file and playwright global config file with m2d1 testcase

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html", { outputFolder: "playwright-report" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,8 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    screenshot:"only-on-failure",
-    
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
@@ -40,14 +39,13 @@ export default defineConfig({
       name: "chromium",
       use: {
         ...devices["Desktop Chrome"],
-       // viewport: { width: 1920, height: 1080 },
+        // viewport: { width: 1920, height: 1080 },
       },
     },
 
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
-
     },
 
     {
